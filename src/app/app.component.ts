9f262e9a60d223b5e2c6b23fc4ce86d061d674f7
ari_fakhri <ari.fakhri@appschef.com>
2017-04-19 13:55:40 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { AngularFire, AuthProviders, AuthMethods,FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+
+	items: FirebaseListObservable<any>;
+	name:any;
+	msgVal:string = '';
+	constructor(public angularFire:AngularFire) {
+		this.items =	angularFire.database.list('/messages', {
+			query:{
+				limitToLast:50
+			}
+		})
+
+		this.angularFire.auth.subscribe(auth => {
+			if (auth) {
+				this.name = auth;
+			}
+		})
+	}
+
+  login(){
+  	this.name = "ari fakhri"
+  	// this.angularFire.auth.login({
+  	// 	provider: AuthProviders.Anonymous,
+  	// 	method: AuthMethods.Anonymous
+  	// });
+  	console.log("mantap pisan")
+  }
+
+  Send(desc:string){
+  	this.items.push({message:desc});
+  	this.msgVal = '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,18 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+// firebase
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAhSmesjjig7EnC3P5R3rWhRk-psfW_9UY",
+  authDomain: "fir-angular-4.firebaseapp.com",
+  databaseURL: "https://fir-angular-4.firebaseio.com",
+  projectId: "fir-angular-4",
+  storageBucket: "fir-angular-4.appspot.com",
+  messagingSenderId: "1090342189002"
+};
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +24,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
